b4e217ed659a67db2c9cd28bdfb9a07def17ce21
pikasuke <tongfongctm@hotmail.com>
2020-06-14 23:39:46 +0200

    Suppression exercice promesse

--- promesse/traitement1.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$_POST => array => $_POST['personne']
-$f = fopen('prenom.txt','a'); //on ouvre le fichier
-fwrite($f,$_POST['personne']); //on écrit sur le fichier
-$f = fclose($f); //on ferme le fichier
-$file = file_get_contents('prenom.txt', true);
-echo $file;
